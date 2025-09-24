073d6636f8415400d7843c722998a053051cfdb4
Ben Johansen <johansenben12@gmail.com>
2025-09-24 11:26:13 -0600

    components folder

---
+++ components/Logo.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import customPathNames from "@/public/images/logos/customPathNames.json";
+
+const pathNames = customPathNames as Record<string, string>;
+
+export default function Logo({
+  name,
+  size,
+  className,
+}: {
+  name: string;
+  size: number;
+  className?: string;
+}) {
+  return (
+    <Image
+      className={className ?? ""}
+      src={`images/logos/${(pathNames[name] as string) ?? name}Logo.svg`}
+      alt={name}
+      width={size}
+      height={size}
+    />
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "no-unused-vars": "off",
-  "prefer-const": ["warn", {
-    "destructuring": "all", // Optionally, apply this rule to destructured variables
-    "ignoreReadBeforeAssign": false
-  }]
 };
 
 export default nextConfig;
